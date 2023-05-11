4ad10639625c4d2727769e807910977b90e09aaa
WSQD3Chimp <70527945+WSQD3Chimp@users.noreply.github.com>
2023-05-10 21:39:16 -0400

    Update EnemtAi.cs

--- The Crimson Awakening/Assets/Scripts/EnemtAi.cs
+++ The Crimson Awakening/Assets/Scripts/EnemtAi.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,13 +26,47 @@ public class EnemryAi : MonoBehaviour
     }
     
     private void Patrolling() {
+        if (!WPset) {
+            FindWWP();
+        } 
+        if (WPset) {
+            NAV.SetDestination(WP);
+        }
+        Vector3 distanceToWP = transform.position - WP;
 
+        if(distanceToWP.magnitude < 1f) {
+            WPset = false;
+        }
     }
+
+    private void FindWWP()
+    {
+        float WPZ = UnityEngine.Random.Range(-WPRange, WPRange);
+        float WPX = UnityEngine.Random.Range(-WPRange, WPRange);
+
+        WP = new Vector3(transform.position.x + WPX, transform.position.y, transform.position.z + WPZ);
+
+        if (Physics.Raycast(WP, -transform.up, 2f, WhatIsGround)) {
+            WPset = true;
+        }
+    }
+
     private void ChasePlayer() {
-
+        NAV.SetDestination(player.position);
     }
-    private void AttackPlayer() {
 
+    private void AttackPlayer() {
+        NAV.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if (!Attacked) {
+            Attacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttack);
+        }
+    }
+
+    private void ResetAttack() {
+        Attacked = false;
     }
 
     // Start is called before the first frame update
@@ -44,5 +79,16 @@ public class EnemryAi : MonoBehaviour
     void Update()
     {
         playerInVisualRange = Physics.CheckSphere(transform.position, visualRange, WhatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, WhatIsPlayer);
+
+        if (!playerInVisualRange && !playerInAttackRange) {
+            Patrolling();
+        }
+        if (playerInVisualRange && !playerInAttackRange) {
+            ChasePlayer();
+        }
+        if  (playerInAttackRange && playerInVisualRange) {
+            AttackPlayer();
+        }
     }
 }

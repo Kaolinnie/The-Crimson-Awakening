42cfe7f8bd81a59cd3ff7c9b296a066c7151db3d
Kaolin <kaolin.stacey@gmail.com>
2023-05-17 19:59:17 -0400

    ui done

--- The Crimson Awakening/Assets/Scripts/Enemy/Enemy.cs
+++ The Crimson Awakening/Assets/Scripts/Enemy/Enemy.cs
@@ -7,21 +7,27 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour {
     public float health = 50;
     private float _maxHealth;
-    public Slider healthSlider;
-    public Canvas healthCanvas;
+    public GameObject healthSlider;
+    public GameObject healthCanvas;
+
+    private Slider _health;
+    private Canvas _canvas;
     
     // Start is called before the first frame update
     void Start() {
-        healthCanvas.renderMode = RenderMode.WorldSpace;
-        healthCanvas.worldCamera = Camera.main;
-        healthSlider.maxValue = _maxHealth;
-        healthSlider.direction = Slider.Direction.LeftToRight;
-        healthSlider.maxValue = health;
+        _health = healthSlider.GetComponent<Slider>();
+        _canvas = healthCanvas.GetComponent<Canvas>();
+        
+        _canvas.renderMode = RenderMode.WorldSpace;
+        _canvas.worldCamera = Camera.main;
+        _health.maxValue = _maxHealth;
+        _health.direction = Slider.Direction.LeftToRight;
+        _health.maxValue = health;
     }
 
     // Update is called once per frame
     void Update() {
-        healthSlider.value = health;
+        _health.value = health;
         if (health <= 0) {
             Destroy(gameObject);
         }
--- The Crimson Awakening/Assets/Singletons/GameManager.cs
+++ The Crimson Awakening/Assets/Singletons/GameManager.cs
@@ -3,25 +3,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-    
-
     // Write down your variables here
-    
     private void Awake()
     {
         Instance = this;
     }
-
-
     // Start is called before the first frame update
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
-
-    // void Update() {
-    //     _player.AdjustHealth(-5.0f);
-    //     Debug.Log("decreasing health");
-    // }
 
 }
